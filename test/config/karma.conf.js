2aebd6588229ac46a0ef3d5359d9a7f717e95fef
Tuan CONG LE <tlcong@linagora.com>
2017-10-04 17:42:55 +0700

    linagora/lgs/openpaas/linagora.esn.admin#249 setting up the environment for unit test frontend

--- test/config/karma.conf.js
+++ test/config/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
       'frontend/components/lodash/dist/lodash.min.js',
       'frontend/components/sinon-chai/lib/sinon-chai.js',
       'frontend/components/sinon-1.15.4/index.js',
-      'test/config/mocks.js',
+      'test/config/mocks/**/*.js',
       'frontend/app/**/*.module.js',
       'frontend/app/**/*.js',
       'frontend/app/**/*.pug'
