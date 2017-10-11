4f95a7e78f02a39504c0294f5cc1c2ab4b55e5fc
Sang NGO DOAN <sngodoan@linagora.com>
2017-10-11 14:33:54 +0700

    linagora/lgs/openpaas/linagora.esn.admin#272 register i18n to webserver

--- backend/webserver/application.js
+++ backend/webserver/application.js
@@ -7,8 +7,7 @@ module.exports = function(dependencies) {
 
   const application = express();
 
-  // Every express new configuration are appended here.
-  // This needs to be initialized before the body parser
+  require('./config/i18n')(dependencies, application);
   require('./config/views')(dependencies, application);
 
   return application;
