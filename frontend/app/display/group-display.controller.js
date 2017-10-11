5037fff9b9d15397a360f198df42f37e3ab7548f
Sang NGO DOAN <sngodoan@linagora.com>
2017-10-11 11:17:00 +0700

    linagora/lgs/openpaas/linagora.esn.admin#272 support update group in group display page

--- frontend/app/display/group-display.controller.js
+++ frontend/app/display/group-display.controller.js
@@ -4,11 +4,18 @@
   angular.module('linagora.esn.group')
     .controller('GroupDisplayController', GroupDisplayController);
 
-  function GroupDisplayController($stateParams, groupApiClient) {
+  function GroupDisplayController(
+    $stateParams,
+    $modal,
+    $scope,
+    groupApiClient,
+    GROUP_EVENTS
+  ) {
     var self = this;
     var groupId = $stateParams.groupId;
 
     self.$onInit = $onInit;
+    self.onEditBtnClick = onEditBtnClick;
 
     function $onInit() {
       groupApiClient
@@ -16,6 +23,27 @@
         .then(function(resp) {
           self.group = resp.data;
         });
+
+      listenOnUpdate();
+    }
+
+    function onEditBtnClick() {
+      $modal({
+        templateUrl: '/group/app/update/group-update.html',
+        backdrop: 'static',
+        placement: 'center',
+        controllerAs: '$ctrl',
+        controller: 'GroupUpdateController',
+        locals: {
+          group: self.group
+        }
+      });
+    }
+
+    function listenOnUpdate() {
+      $scope.$on(GROUP_EVENTS.GROUP_UPDATED, function(event, group) {
+        self.group = group;
+      });
     }
   }
 })(angular);
