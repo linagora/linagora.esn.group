554534cc3c968fecc053de29663a6b5c5933e37e
Christophe Hamerling <chamerling@linagora.com>
2018-10-03 09:51:43 +0200

    linagora/lgs/openpaas/esn#2502 Remove the item from the application menu

--- frontend/app/app.run.js
+++ frontend/app/app.run.js
@@ -3,16 +3,8 @@
 
   angular.module('linagora.esn.group')
 
-  .run(function(esnMemberResolverRegistry, dynamicDirectiveService, session, groupMemberResolverService, GROUP_OBJECT_TYPE) {
-    session.ready.then(function() {
-      if (session.userIsDomainAdministrator()) {
-        var group = new dynamicDirectiveService.DynamicDirective(true, 'group-application-menu', { priority: -10 });
-
-        dynamicDirectiveService.addInjection('esn-application-menu', group);
-      }
-
-      esnMemberResolverRegistry.addResolver({ objectType: GROUP_OBJECT_TYPE, resolve: groupMemberResolverService });
-    });
+  .run(function(esnMemberResolverRegistry, groupMemberResolverService, GROUP_OBJECT_TYPE) {
+    esnMemberResolverRegistry.addResolver({ objectType: GROUP_OBJECT_TYPE, resolve: groupMemberResolverService });
   })
 
   .run(function(attendeeService, groupAttendeeProvider) {
